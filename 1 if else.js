b03139d350e3565f0d25a17fd1f1ff8fcd9f8ef7
wildangeek <54316377+wildangeek@users.noreply.github.com>
2020-06-11 15:28:37 +0700

    Update 1 if else.js

--- 1 if else.js
+++ 1 if else.js
@@ -2,7 +2,7 @@ let karyawan = 'ceo'
 if(karyawan === 'hr'){
     console.log('hire pegawai')
 }else if(karyawan === 'manager'){
-  console.log('manager bawahan')
+  console.log('manage bawahan')
 }else if(karyawan ==='ceo'){
   console.log('pantau company growth')
 }
